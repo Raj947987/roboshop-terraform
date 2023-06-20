755d0ff83e968017d2d01345a7d397eeabd036c1
Raja Bhushnam <rajabhushanam117@gmail.com>
2023-06-20 23:46:34 +0530

    Include_tasks in to another ansible file

--- env-dev/main.tfvars
+++ env-dev/main.tfvars
@@ -1,1 +1,15 @@
 env = "dev"
+
+components = {
+
+    frontend = {}
+    mongodb = {}
+    catalogue = {}
+    redis = {}
+    cart = {}
+    user = {}
+    shipping = {}
+    rabbitmq = {}
+    mysql = {}
+    payment = {}
+  }
--- main.tf
+++ main.tf
@@ -1,4 +1,11 @@
-module "test" {
+#module "test" {
+#  source = "git::https://github.com/Raj947987/tf-module-app.git"
+#  env = var.env
+#}
+
+module "instances" {
+  for_each = var.components
   source = "git::https://github.com/Raj947987/tf-module-app.git"
+  component = each.key
   env = var.env
 }
